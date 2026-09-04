d241af842209c04e4470a462159b46550b6d8fb0
aqoprojectsdev <aqoprojectsdev@gmail.com>
2026-09-04 23:54:01 +0100

    feat: homepage, hose search page, house detail page, contact, about us

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Homepage from './Homepage.jsx'
+// import Homepage from './Homepage.jsx'
 import LandingPage from './Pages/LandingPage.jsx'
 import HousesPage from './Pages/HousesPage.jsx'
 import HousePage from './Pages/HousePage'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <BrowserRouter>
